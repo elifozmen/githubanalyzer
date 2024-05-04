994e847e49011b39bf65e5470821a09f4acfd71c
elifozmen <80031128+elifozmen@users.noreply.github.com>
2024-05-04 19:34:36 +0300

    bar chart eklendi

---
+++ src/BarChart/BarChart.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart, registerables } from 'chart.js';
+import { options } from './BarChartOptions'; // Bar grafiği için özel seçenekler
+import axios from 'axios';
+
+Chart.register(...registerables);
+
+const BarGraph = () => {
+  const [chartData, setChartData] = useState(null);
+
+  useEffect(() => {
+    // Fetch chart data when component mounts
+    getChartData();
+  }, []);
+
+  const getChartData = () => {
+    axios.get('http://localhost:5001/get-chart-data')
+      .then(response => {
+        console.log(response.data);
+        const { labels, datapoints } = response.data;
+        const months = labels;
+        const graphData = datapoints;
+        const updatedData = {
+          labels: months,
+          datasets: [
+            {
+              backgroundColor: '#5052ff',
+              data: graphData,
+              barPercentage: 1,
+              borderRadius: 100,
+              borderSkipped: true,
+            },
+          ],
+        };
+        setChartData(updatedData);
+      })
+      .catch(error => {
+        console.error('Error getting chart data:', error);
+      });
+  };
+
+  // Grafik stilini tanımla
+  const graphStyle = {
+    minHeight: '10rem',
+    maxWidth: '540px',
+    width: '100%',
+    border: '1px solid #C4C4C4',
+    borderRadius: '0.375rem',
+    padding: '1rem',
+  };
+
+  return (
+    <div style={graphStyle}>
+      {/* Bar grafiği bileşeni */}
+      {chartData && <Bar data={chartData} options={options} />}
+    </div>
+  );
+};
+
+export default BarGraph;
--- src/workload.js
+++ src/workload.js
@@ -1,33 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from './Navbar';
 import './styles.css';
+import axios from 'axios';
+import { Line } from 'react-chartjs-2'; // Import Chart.js Line component
+import BarGraph from './BarChart/BarChart';
 
 function Workload() {
-  const [distributionImage, setDistributionImage] = useState(null);
-
-  useEffect(() => {
-    // Fetch the distribution image when the component mounts
-    fetch('/get-distribution')
-      .then(response => response.text())
-      .then(data => {
-        // Set the image data to state
-        setDistributionImage(data);
-      })
-      .catch(error => console.error('Error fetching distribution image:', error));
-  }, []); // Empty dependency array ensures the effect runs only once
+  
 
   return (
     <div>
       <Navbar />
-      <div>
-        {distributionImage ? (
-          // Display the distribution image if available
-          <img src={distributionImage} alt="Distribution" />
-        ) : (
-          // Display a loading message if the image is still loading
-          <p>Loading distribution...</p>
-        )}
-      </div>
+      <BarGraph/>
     </div>
   );
 }
