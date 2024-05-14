637f99d139949fa4aa957b3c27a39950ac189e97
Ece Karataş <ece.karatas@ozu.edu.tr>
2024-05-14 23:08:46 +0300

    fixed matrix

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState,useLayoutEffect, useEffect } from 'react';
 import axios from 'axios';
 import Navbar from './Navbar.js'; // Import Navbar component
 import './styles.css';
@@ -10,6 +10,7 @@ import BarGraph3 from './BarChart/BarChart3';
 import BarGraph5 from './BarChart/BarChart5';
 import { TypeAnimation } from 'react-type-animation';
 import "swiftie/midnights.css"
+
 
 
 
@@ -334,7 +335,35 @@ function Home() {
       )};
       
 
-
+      <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2024 <a className="hover:underline">Github Analyzer™</a>. All Rights Reserved.
+          </span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                Licensing
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
 
     </div>
   );
